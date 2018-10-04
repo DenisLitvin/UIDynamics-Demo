84a47f3dc177c4f9fd38ae9132ba3cf502395cf0
Denis Litvin <den.litvinn@gmail.com>
2018-10-04 12:00:28 -0500

    Initial Commit

---
+++ UIDynamics Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIDynamics Demo
+//
+//  Created by Denis Litvin on 10/4/18.
+//  Copyright © 2018 Denis Litvin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
